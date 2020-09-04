8df424c48ff1a90e460dab94ab20e50b8af8365f
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-09-04 09:50:25 -0300

    show data

--- src/br/edu/insper/Exemplo.java
+++ src/br/edu/insper/Exemplo.java
@@ -1,5 +1,4 @@
 package br.edu.insper;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -37,18 +36,35 @@ public class Exemplo extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-	
-	protected void service (HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String curso = request.getParameter("curso");
+		PrintWriter out = response.getWriter();
+		
         out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("</head>");
         out.println("<body>");
-        out.println("Servlet funcionando");
+        out.println("<h1>Confirme os dados informados</h1>");
+        out.println(" <hr/>");
+        out.println("Nome: " + nome + "<br/>");
+        out.println("Email: " + email + "<br/>");
+        out.println("Curso: " + curso + "<br/>");
+        out.println("<button>Confirmar</button>");
         out.println("</body>");
         out.println("</html>");
 	}
+	
+//	protected void service (HttpServletRequest request,
+//            HttpServletResponse response)
+//            throws ServletException, IOException {
+//        PrintWriter out = response.getWriter();
+//        out.println("<html>");
+//        out.println("<body>");
+//        out.println("Servlet funcionando");
+//        out.println("</body>");
+//        out.println("</html>");
+//	}
 
 }
