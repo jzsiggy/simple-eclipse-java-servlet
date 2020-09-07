eb3fd1163dcf6a64d28dcef589a268a36c700362
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-09-07 12:36:37 -0300

    hotFix

--- src/br/edu/insper/Exemplo.java
+++ src/br/edu/insper/Exemplo.java
@@ -56,15 +56,6 @@ public class Exemplo extends HttpServlet {
         out.println("</html>");
 	}
 	
-//	protected void service (HttpServletRequest request,
-//            HttpServletResponse response)
-//            throws ServletException, IOException {
-//        PrintWriter out = response.getWriter();
-//        out.println("<html>");
-//        out.println("<body>");
-//        out.println("Servlet funcionando");
-//        out.println("</body>");
-//        out.println("</html>");
-//	}
+
 
 }
